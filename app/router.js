34fcfc54ad528c37a4c707faa5725a8095972ec2
Micah Caffey <mrcaffey@gmail.com>
2019-02-13 15:55:38 -0700

    added testimonials page

---
+++ app/router.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Dimensions, Platform } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import Profile from './screens/Profile';
+
+let screen = Dimensions.get('window');
+
+export const Tabs = TabNavigator({
+  'Profile': {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({ tintColor }) => <Icon name="ios-person-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+});
+
+export const createRootNavigator = () => {
+  return StackNavigator(
+    {
+      Tabs: {
+        screen: Tabs,
+        navigationOptions: {
+          gesturesEnabled: false
+        }
+      }
+    },
+  )
+  };
